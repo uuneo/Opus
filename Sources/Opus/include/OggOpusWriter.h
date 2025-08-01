980a2354c6395ad0909468c1f0cb6dc9fc6cf9c6
uuneo <uuneo@qq.com>
2025-08-01 12:51:12 +0800

    初始

---
+++ Sources/Opus/include/OggOpusWriter.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class DataItem;
+
+@interface OggOpusWriter : NSObject
+
+- (bool)beginWithDataItem:(DataItem *)dataItem;
+- (bool)beginAppendWithDataItem:(DataItem *)dataItem;
+
+- (bool)writeFrame:(uint8_t * _Nullable)framePcmBytes frameByteCount:(NSUInteger)frameByteCount;
+- (NSUInteger)encodedBytes;
+- (NSTimeInterval)encodedDuration;
+
+- (NSDictionary *)pause;
+- (bool)resumeWithDataItem:(DataItem *)dataItem encoderState:(NSDictionary *)state;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/Opus/include/OpusBinding.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+#import <OpusBinding/OggOpusReader.h>
+#import <OpusBinding/DataItem.h>
+#import <OpusBinding/OggOpusWriter.h>
